349a123b950b1137f21471ad0b39be7c418a7a96
Carlo Ramponi <magocarlos1999@gmail.com>
2025-12-10 01:09:28 +0100

    Setup PMP to "allow all" and switch to U mode

--- software/hello/hello.c
+++ software/hello/hello.c
@@ -4,35 +4,34 @@
 #include <stdio.h>
 #include <metal/pmp.h>
 
+#define CONFIG_TO_INT(_config) (*((char *)&(_config)))
+
 extern void switch_to_user_mode();
 
 void set_pmp() {
-    struct metal_pmp *pmp = metal_pmp_get_device();
-    if(!pmp) {
-        printf("No PMP device found\n");
-        return;
-    }
-    metal_pmp_init(pmp);
     
-    // Set PMP to allow all access to all memory regions
-    metal_pmp_set_region(
-        pmp,
-        0,
-        (struct metal_pmp_config){
-            .R=1,
-            .W=1,
-            .X=1,
-            .A=METAL_PMP_NAPOT,
-            .L=METAL_PMP_UNLOCKED
-        },
-        0xFFFFFFFF
-    );
+    struct metal_pmp_config cfg = {
+        .R=1,
+        .W=1,
+        .X=1,
+        .A=METAL_PMP_NAPOT,
+        .L=METAL_PMP_UNLOCKED
+    };
+
+    uint32_t address = 0xFFFFFFFF;
+    unsigned int region = 0;
+    size_t cfgmask = (0xFF << (8 * (region % 4)));
+    size_t pmpcfg = (CONFIG_TO_INT(cfg) << (8 * (region % 4)));
+    
+    __asm__("csrw pmpaddr0, %[addr]" ::[addr] "r"(address) :);
+    __asm__("csrc pmpcfg0, %[mask]" ::[mask] "r"(cfgmask) :);
+    __asm__("csrs pmpcfg0, %[cfg]" ::[cfg] "r"(pmpcfg) :);
 }
 
 int main() {
 
     printf("Setting the PMP configuration...\n");
-    // set_pmp();
+    set_pmp();
 
     printf("Trying to switch to user mode...\n");
     switch_to_user_mode();
